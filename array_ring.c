99b5bfd9659a3109887a9224a2ec3066b35464d6
Ya <Ya@Yas-MacBook-Pro.fios-router.home>
2017-03-30 21:58:35 -0400

    irecv/isend

--- array_ring.c
+++ array_ring.c
@@ -1,4 +1,4 @@
-/* Communication ping-pong:
+/* Communication ring with non_blocking communication:
  * pass a 2MB message in a ring
  * 0 -> 1 -> 2 -> 3 -> .... -> 0
  */
@@ -41,18 +41,59 @@ int main( int argc, char *argv[])
   for(i = 0; i < N; i++){
     if(rank !=0){
       origin = rank -1;
-      MPI_Recv(&message,  M, MPI_DOUBLE, origin,      tag, MPI_COMM_WORLD, &status);
+      MPI_Irecv(&message,  M, MPI_DOUBLE, origin,      tag, MPI_COMM_WORLD, &status);
       printf("rank %d/%d hosted on %s received array from %d \n", rank,size, hostname, origin);
     }	
-    MPI_Send(&message, M, MPI_DOUBLE, (rank+1)%size, tag, MPI_COMM_WORLD);
+    MPI_Isend(&message, M, MPI_DOUBLE, (rank+1)%size, tag, MPI_COMM_WORLD);
   
     if(rank == 0)
     {
       origin = size - 1;
-      MPI_Recv(&message,  M, MPI_DOUBLE, origin,      tag, MPI_COMM_WORLD, &status);
+      MPI_Irecv(&message,  M, MPI_DOUBLE, origin,      tag, MPI_COMM_WORLD, &status);
       printf("rank %d/%d hosted on %s received array from %d \n", rank,size, hostname, origin);
     }
   }
+  
+  /*
+   * Do work
+   */
+  {
+    int i;
+    double x = 0;
+
+    for(i = 0; i < 100000; ++i)
+    {
+      int sign = (i%2) ? -1 : 1;
+      x += sign * 1.0/(2*i+1);
+    }
+
+    printf("%d: %25.16g\n", rank, 4*x);
+  }
+
+  /*
+   * Wait for incomming message
+   */
+  MPI_Wait(&request_in, &status);
+  /*
+   * Do work
+   */
+  {
+    int i;
+    double x = 0;
+
+    for(i = 0; i < 100000; ++i)
+    {
+      int sign = (i%2) ? -1 : 1;
+      x += sign * 1.0/(2*i+1);
+    }
+
+    printf("%d: %25.16g\n", rank, 4*x);
+  }
+
+  /*
+   * Wait for outgoing message
+   */
+  MPI_Wait(&request_out, &status);
 
   get_timestamp(&time2);
   double elapsed = timestamp_diff_in_seconds(time1,time2);
